bddb454aadb43e2216f1f09a8f536da620fc25e3
My Name <harika.raj@hotmail.com>
2021-06-30 00:33:44 -0500

    worked on flask completion

--- app.py
+++ app.py
@@ -23,19 +23,19 @@ def index():
     f"/api/v1.0/precipitation <br>"
     f"/api/v1.0/stations <br>"
     f"/api/v1.0/tobs <br>"
-    f"/api/v1.0/<start> <br>"
-    f"/api/v1.0/<start>/<end>"
+    f"/api/v1.0/start <br>"
+    f"/api/v1.0/start/end"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
     # Design a query to retrieve the last 12 months of precipitation data and plot the results
-    Latest_date = session.query(func.max(Measurement.date)).first()
+    # Latest_date = session.query(func.max(Measurement.date)).first()
     # year = Latest_date.strftime("%Y")
     # month = Latest_date.strftime("%-m")
     # day = Latest_date.strftime("%d")
-
+    # print(month)
     # Calculate the date 1 year ago from the last data point in the database
     previousyr_date = dt.date(2017,8,23)-dt.timedelta(days=365)
       
@@ -66,9 +66,37 @@ def active():
     return(jsonify(active_station_data))
 
 @app.route("/api/v1.0/<start>")
-def start_date(start):
+@app.route("/api/v1.0/<start>/<end>")
+
+def calc_temps(start=None, end=None):
+    """TMIN, TAVG, and TMAX for a list of dates.
     
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    session = Session(engine)
+    if not end:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all() 
+
+        temperature = list(np.ravel(results))
+
+        return jsonify(temperature)
+
+    if end:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+        temperature = list(np.ravel(results))
+
+        return jsonify(temperature)
+    session.close()
 
 
 if __name__== "__main__":
     app.run(debug=True)
+
